6dbe943058daffc4191be63f8eae70c747bc440b
Alina <alina90565@gmail.com>
2019-10-20 15:14:31 +0300

    Updates

---
+++ precache-manifest.7736b15328646c5d062d095d915f403a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da080c7c8e23392ab66b0b01e1123903",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "ee69190c23847631d9df",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "cc8f6abf1074df2b3565",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.594bfab1.chunk.css"
+  },
+  {
+    "revision": "ee69190c23847631d9df",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.9d40fc03.chunk.js"
+  },
+  {
+    "revision": "cc8f6abf1074df2b3565",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.d822799f.chunk.js"
+  },
+  {
+    "revision": "1f5ad8073d8eccbd6674",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.50193cb7.js"
+  }
+]);
